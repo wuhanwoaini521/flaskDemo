458306524b12162fabf3bc512abcc9462c87ed34
wuhanwoaini521 <1319817190@qq.com>
2021-11-23 14:24:04 +0800

    测试 模板

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask,escape,render_template
 
 app = Flask(__name__)
 
+name = 'Wu Han'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
+@app.route('/index')
+@app.route('/home')
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
+@app.route("/user/<name>")
+def user_page(name):
+    return "User's :%s " % escape(name)
 
 
 if __name__ == '__main__':
